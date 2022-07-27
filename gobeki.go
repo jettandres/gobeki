15f548a5470512ff6707dccc64d5ef598b7244e7
Jett Robin Andres <jettrobin.andres@gmail.com>
2022-07-27 21:04:22 +0800

    Pass failing test with loop

--- gobeki.go
+++ gobeki.go
@@ -1,8 +1,18 @@
 package gobeki
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"strings"
+)
 
-func gobeki(length int) string {
-	fmt.Print(length)
-	return "hello world"
+// gobeki generate beki ipsum sentence
+// params:
+//   numWords - number of words to generate
+func gobeki(numWords int) string {
+	buf := bytes.Buffer{}
+	for i := 0; i < numWords; i++ {
+		fmt.Fprint(&buf, "hello ")
+	}
+	return strings.TrimSuffix(buf.String(), " ")
 }
